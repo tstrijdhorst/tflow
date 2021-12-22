42c32227f9966f626d2cdcbf7a6758dbdd1c7b1b
Tim Strijdhorst <tim@decorrespondent.nl>
2021-12-22 16:54:33 +0100

    +function to create git branch, unsure why we need this convoluted plumbing method instead of Repository.CreateNewBranch() but it just doesnt work...

--- jflow.go
+++ jflow.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/spf13/viper"
 	"gopkg.in/andygrunwald/go-jira.v1"
+	"os"
 )
 
 func main() {
 	initConfig()
+	createNewGitBranch("lol")
 }
 
 func initConfig() {
@@ -17,6 +21,22 @@ func initConfig() {
 	if err != nil {
 		panic(fmt.Errorf("Fatal error in config file: %w \n", err))
 	}
+}
+
+func createNewGitBranch(name string) {
+	workingDirectory, _ := os.Getwd()
+
+	r, err := git.PlainOpen(workingDirectory)
+
+	if err != nil {
+		panic(fmt.Errorf("Fatal error in git repo: %w \n", err))
+	}
+
+	headRef, _ := r.Head()
+	ref := plumbing.NewHashReference(plumbing.ReferenceName(fmt.Sprintf("refs/heads/%v", name)), headRef.Hash())
+
+	// The created reference is saved in the storage.
+	err = r.Storer.SetReference(ref)
 }
 
 func getJiraIssueSummary(id string) string {
