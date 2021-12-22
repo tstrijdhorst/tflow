a843d7448ceb0c6c5c262dd2f3e901b3c17c942f
Tim Strijdhorst <tim@decorrespondent.nl>
2021-12-22 16:26:40 +0100

    +function to get the summary for a given jira issue-id

--- jflow.go
+++ jflow.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"gopkg.in/andygrunwald/go-jira.v1"
 )
 
 func main() {
 	initConfig()
-	fmt.Printf("%v", viper.Get("jira.api-key"))
 }
 
 func initConfig() {
@@ -17,4 +17,23 @@ func initConfig() {
 	if err != nil {
 		panic(fmt.Errorf("Fatal error in config file: %w \n", err))
 	}
+}
+
+func getJiraIssueSummary(id string) string {
+	tp := jira.BasicAuthTransport{
+		Username: viper.GetString("jira.username"),
+		Password: viper.GetString("jira.token"),
+	}
+
+	client, err := jira.NewClient(tp.Client(), viper.GetString("jira.url"))
+
+	//@todo how to recognize auth failure since apparently it doesnt return an error for that
+
+	if err != nil {
+		fmt.Printf("Error: %w", err)
+	}
+
+	issue, _, err := client.Issue.Get(id, nil)
+
+	return issue.Fields.Summary
 }
