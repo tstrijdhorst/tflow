7326a476f79f5881a67053d2fdca034241abef6d
Tim Strijdhorst <tim@decorrespondent.nl>
2022-01-06 16:11:12 +0100

    %update references

--- cmd/branch.go
+++ cmd/branch.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/tstrijdhorst/JFlow/services"
+	"github.com/tstrijdhorst/tflow/services"
 )
 
 var branchCmd = &cobra.Command{
